76bb35589fe259265d245a21587ee00dad08398a
mgarreaza13 <mg.arreaza.13@gmail.com>
2020-02-27 17:25:57 -0400

    init first external services

---
+++ ubank-test/src/app/core/services/teams.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable()
+export class TeamsService {
+  constructor(private http: HttpClient) {}
+
+
+
+  /** get all scores finished from a team 
+   * @param id id team
+   */
+  getScheduledTeam(id?){
+    return this.http.get(`https://api.football-data.org/v2/teams/86/matches?season=2019/matches?status=FINISHED`);
+  }
+}
